5cba6da682ecdc2ca373e171ef0cc6dc8a25baef
Juno Kim <lippoint.surf0622@gmail.com>
2022-06-22 17:32:12 +0900

    안드로이드 결제 창 뒤로가기 버튼 활성화 처리

--- src/page/Payment.js
+++ src/page/Payment.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ActivityIndicator,
   View,
   Alert,
-  BackHandler
-} from 'react-native'
-import IMP from 'iamport-react-native'
-import { useSelector } from 'react-redux'
+  BackHandler,
+} from 'react-native';
+import IMP from 'iamport-react-native';
+import {useSelector} from 'react-redux';
 
 const Payment = props => {
-  const { route, navigation } = props
-  const { params } = route
-  const { currRoute } = useSelector(state => state.routeReducer)
+  const {route, navigation} = props;
+  const {params} = route;
+  const {currRoute} = useSelector(state => state.routeReducer);
 
-  console.log('Payment params ??', params)
+  console.log('Payment params ??', params);
 
   const Loading = () => (
     <View
@@ -22,12 +22,11 @@ const Payment = props => {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#ffffff'
-      }}
-    >
-      <ActivityIndicator color='#0085CA' size='large' />
+        backgroundColor: '#ffffff',
+      }}>
+      <ActivityIndicator color="#0085CA" size="large" />
     </View>
-  )
+  );
   /* [필수입력] 결제에 필요한 데이터를 입력합니다. */
   const data = {
     pg: params.pg,
@@ -43,34 +42,34 @@ const Payment = props => {
     app_scheme: 'palroinApp',
     customer_uid: params?.customer_uid,
     digital: false,
-    m_redirect_url: params.m_redirect_url
-  }
+    m_redirect_url: params.m_redirect_url,
+  };
 
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
   const callback = response => {
     // console.log(response);
 
     const params = {
       response,
-      type: 'payment'
-    }
+      type: 'payment',
+    };
 
-    console.log('payment response ::', response)
+    console.log('payment response ::', response);
 
     if (response) {
       // navigation.replace('HomePage', params)
 
       if (response.imp_success === 'true') {
-        navigation.replace('PaymentResult', params)
+        navigation.replace('PaymentResult', params);
       }
 
       if (response.imp_success === 'false') {
-        params.type = 'paymentError'
-        navigation.replace('Home', params)
+        params.type = 'paymentError';
+        navigation.replace('Home', params);
       }
     }
-  }
+  };
 
   // React.useEffect(() => {
   //   if (currRoute.name === 'Payment') {
@@ -96,41 +95,41 @@ const Payment = props => {
   //   }
   // });
 
-  const backAction = () => {
-    return true
-    // Alert.alert(
-    //   '결제를 취소하시겠습니까?',
-    //   '',
-    //   [
-    //     { text: '아니요' },
-    //     {
-    //       text: '네',
-    //       onPress: () => navigation.goBack()
-    //     }
-    //   ],
-    //   {
-    //     cancelable: true
-    //   }
-    // )
-  };
+  // const backAction = () => {
+  //   return true
+  //   // Alert.alert(
+  //   //   '결제를 취소하시겠습니까?',
+  //   //   '',
+  //   //   [
+  //   //     { text: '아니요' },
+  //   //     {
+  //   //       text: '네',
+  //   //       onPress: () => navigation.goBack()
+  //   //     }
+  //   //   ],
+  //   //   {
+  //   //     cancelable: true
+  //   //   }
+  //   // )
+  // };
 
-  BackHandler.addEventListener('hardwareBackPress', backAction)
+  // BackHandler.addEventListener('hardwareBackPress', backAction)
 
-  console.log('visible ??', visible)
+  console.log('visible ??', visible);
 
   return (
-    <SafeAreaView style={[{ flex: 1, backgroundColor: '#fff' }]}>
+    <SafeAreaView style={[{flex: 1, backgroundColor: '#fff'}]}>
       <IMP.Payment
         userCode={params?.usercode}
         loading={<Loading />}
         data={{
           ...data,
-          app_scheme: 'palroinApp'
+          app_scheme: 'palroinApp',
         }}
         callback={callback}
       />
     </SafeAreaView>
-  )
+  );
 };
 
-export default Payment
+export default Payment;
